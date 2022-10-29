c8ec89da150129ea964f0b621912e7758efbb280
AmimerNabil <amimernabil@gmail.com>
2022-10-29 14:00:36 -0400

    here you go

--- chap-website/pages/_app.js
+++ chap-website/pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css';
+// import SSRProvider from "react-bootstrap/SSRProvider";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from '../component/Layout/Layout';
 
 function MyApp({ Component, pageProps }) {
