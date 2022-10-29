bacd4ad57d63ca5e814a012331f3c6fb301ba000
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-10-29 13:57:31 -0400

    layout

---
+++ chap-website/component/Layout/Layout.js
@@ -0,0 +1,15 @@
+import NaviBar from "./NaviBar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+    return(
+        <div>
+            <NaviBar />
+            { children }
+            <Footer />
+        </div>
+
+    );
+}
+
+export default Layout;
