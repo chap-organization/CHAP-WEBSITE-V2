3e68d9d24b4fe2722c67e02cfc45c116b7e384a0
AmimerNabil <amimernabil@gmail.com>
2022-10-30 00:14:44 -0400

    adding modal

--- chap-website/component/Layout/NaviBar.js
+++ chap-website/component/Layout/NaviBar.js
@@ -3,35 +3,102 @@ import Navbar from 'react-bootstrap/Navbar';
 import Image from "next/image";
 import logo from '../../public/assets/CHAP.png';
 import styles from '../../styles/component/Layout/NaviBar.module.css';
-import { Button, Container } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+import { useEffect, useState } from 'react';
 
 const NaviBar = () => {
-    return (
-        <Navbar sticky="top" collapseOnSelect expand="lg" className={`${styles.ourNavbar}`}>
-            <Navbar.Brand>
-                <Image
-                    className={styles.logo}
-                    src={logo}
-                    width={317}
-                    height={67}
-                />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-conntrol='navbarScroll' data-bs-target='navbarScroll'/>
-            <Navbar.Collapse id="navbarScroll" className={`justify-content-end ${styles.navContent}`}>
-                <Nav className={`${styles.navlinks}`}>
-                    <Nav.Link href="/#About">About</Nav.Link>
-                    <Nav.Link href="/#OurTeam">Our Team</Nav.Link>
-                    <Nav.Link href="/#ProjectsAndAward">Projects & Awards</Nav.Link>
-                </Nav>
-                <Nav className={`${styles.joinUs}`}>
-                    <Button className={styles.joinUsButton}>
-                        Join Us
-                    </Button>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
-}
 
+    const [showModal, setShowModal] = useState(false);
+
+    const handleModalClick = () => {
+        setShowModal(!showModal);
+        return;
+    }
+
+    const MyNavBar = ({ buttonEnabled, navBarFix }) => {
+        return (
+            <Navbar fixed={navBarFix == 'fixed' ? 'top' : null} sticky={navBarFix == 'fixed' ? null : 'top'} collapseOnSelect expand="lg" className={`${styles.ourNavbar}`}>
+                <Navbar.Brand>
+                    <Image
+                        alt="CHAP logo"
+                        className={styles.logo}
+                        src={logo}
+                        width={317}
+                        height={67}
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle data-bs-target='navbarScroll' />
+                <Navbar.Collapse id="navbarScroll" className={`justify-content-end ${styles.navContent}`}>
+                    <Nav className={`${styles.navlinks}`}>
+
+                        <Nav.Link style={{
+                            pointerEvents: buttonEnabled
+                        }} href="/#About">About</Nav.Link>
+
+                        <Nav.Link style={{
+                            pointerEvents: buttonEnabled
+                        }} href="/#OurTeam">Our Team</Nav.Link>
+
+                        <Nav.Link style={{
+                            pointerEvents: buttonEnabled
+                        }} href="/#ProjectsAndAward">Projects & Awards</Nav.Link>
+
+                    </Nav>
+                    <Nav className={`${styles.joinUs}`}>
+                        <Button className={`${styles.joinUsButton}`} onClick={handleModalClick}>
+                            Join Us
+                        </Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+
+    const JoinUsModal = () => {
+        return (
+            <div className={`${styles.modalWrapper}`}>
+                <div className={`container ${styles.contentWrapper}`}>
+                    <div className={`${styles.join} text-left`}>Join Us</div>
+                    <select class="form-select" id="inputGroupSelect01">
+                        <option selected>Please Select Inquiry Type</option>
+                        <option value="1">Become a Member</option>
+                        <option value="2">Become a Sponsor</option>
+                        <option value="3">General Inquiry</option>
+                    </select>
+                    <div className={`mt-5 row gx-3`}>
+                        <div className={`mt-3 col-lg-6`}>
+                            <input type="text" className={`${styles.forminput} form-control`}  placeholder="Last Name" aria-describedby="addon-wrapping" />
+                        </div>
+                        <div className={`mt-3 col-lg-6`}>
+                            <input type="text" className={`${styles.forminput} form-control`}  placeholder="First Name" aria-describedby="addon-wrapping" />
+                        </div>
+                    </div>
+                    <div className={`mt-3 row gx-3`}>
+                        <div className={`mt-3 col-lg-6`}>
+                            <input type="text" className={`${styles.forminput} form-control`}  placeholder="Email" aria-describedby="addon-wrapping" />
+                        </div>
+                        <div className={`mt-3 col-lg-6`}>
+                            <input type="text" className={`${styles.forminput} form-control`} placeholder="Phone" aria-describedby="addon-wrapping" />
+                        </div>
+                    </div>
+                    <textarea className={`${styles.textContent} ${styles.forminput}  mt-5 form-control`} placeholder='Message...'></textarea>
+
+                    <div className='row mt-5'>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    return showModal ? (
+        <div>
+            <JoinUsModal />
+            <MyNavBar navBarFix='fixed' buttonEnabled={'none'} />
+        </div>
+    ) : (<MyNavBar navBarFix='sticky' buttonEnabled={'all'} />);
+
+}
 
 export default NaviBar;
--- chap-website/pages/index.js
+++ chap-website/pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import CHAP from './CHAP.js'
-import About from './About.js'
+import CHAP from '../component/CHAP/CHAP_comp.js'
+import About from '../component/About/About.js'
 
 export default function Home() {
   return (
