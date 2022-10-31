a7df79e0651738da05ca51e817f09e801d1b0748
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-10-31 14:07:12 -0400

    team section

--- chap-website/component/Team/Team.js
+++ chap-website/component/Team/Team.js
@@ -1,21 +1,26 @@
 import styles from "../../styles/component/Team/Team.module.css";
 import TeamCard from "./TeamCard";
+import TeamStorage from "../../storage/TeamStorage";
+import { useEffect, useState } from "react";
 
 export default function Team() {
 
-    const memberInfo = {
-        name: "Matthew Cabral",
-        role: "Co-President",
-        year: "U2",
-        program: "Software Engineering",
-        linkedin: "/",
-        website: "https://matthew-cabral.com",
-        description: "hey how are u, im a munin",
-    }
+    const [memberInfo, setMemberInfo] = useState(TeamStorage());
 
     return(
-        <section>
-            <TeamCard info={memberInfo}/>
+        <section className={styles.teamSection}>
+            <div className="row">
+                <h3 className={styles.title}>Meet the Team!</h3>
+            </div>
+            <div className="row g-3">
+                {memberInfo.map(info => {
+                    return(
+                        <div key={info.id} className="col-3 centerItem">
+                            {(info.id != 3) &&  <TeamCard info={info}/>}
+                        </div>
+                    );
+                })}
+            </div>
         </section>
     );
 }
